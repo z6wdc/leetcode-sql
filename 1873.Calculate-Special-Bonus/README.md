2908a14b7af7e1758a010bebedc6db9199d5355c
z6wdc <whitedeathcross@gmail.com>
2022-12-05 15:39:13 +0900

    1873

---
+++ 1873.Calculate-Special-Bonus/README.md
@@ -0,0 +1,8 @@
+# Problem 1873. Calculate Special Bonus
+
+- [link](https://leetcode.com/problems/calculate-special-bonus/)
+
+## Solution
+
+`CASE` 式
+`LIKE` 句
---
+++ 1873.Calculate-Special-Bonus/mysql.sql
@@ -0,0 +1,8 @@
+SELECT employee_id,
+CASE 
+WHEN employee_id % 2 = 0 THEN 0
+WHEN name LIKE 'M%' THEN 0
+ELSE salary
+END AS bonus
+FROM Employees
+ORDER BY employee_id
